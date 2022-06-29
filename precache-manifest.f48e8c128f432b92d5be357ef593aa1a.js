49a5b8eac7fa76a4088049057c1765b5533f48ec
Aadilkhan2001 <aadilkhanpathan2001@gmail.com>
2022-06-29 22:41:29 +0530

    Updates

---
+++ precache-manifest.f48e8c128f432b92d5be357ef593aa1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca21b240117fbe46e69d",
+    "url": "/TextUtils/static/css/main.06e12e0e.chunk.css"
+  },
+  {
+    "revision": "ca21b240117fbe46e69d",
+    "url": "/TextUtils/static/js/main.ca21b240.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/TextUtils/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "14506651abc0b1064f2d",
+    "url": "/TextUtils/static/js/runtime~main.14506651.js"
+  },
+  {
+    "revision": "51d7007202a007b0388b7be53c7cc594",
+    "url": "/TextUtils/index.html"
+  }
+];
